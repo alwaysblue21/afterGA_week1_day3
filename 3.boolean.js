a25d0a5ac2e2db058978d3b641963cc8ea2b96b0
Je Yun <alwaysblue21@gmail.com>
2023-12-07 07:55:38 -0500

    booleans

---
+++ 3.boolean.js
@@ -0,0 +1,30 @@
+// = one equal means im going to set it to that value afterwards
+// let name = 'David'
+
+
+// === checks for equal value and equal type
+console.log(1 === 1)
+
+// == checks for equal value 
+console.log("1" == 1)
+
+
+// > greater then check
+// >= "greater than or equal" to check
+console.log(1 > 1) 
+console.log(1 >= 1)
+
+// < less then check
+// <= "less than or equal" to check
+console.log(-1 < -1) 
+console.log(-1 <= -1)
+
+// !== check for NON equality (for type and value)
+console.log(1 !== 1)
+console.log(1 !== 2)
+
+// != check for NON equality (for value)
+console.log(1 != 1)
+console.log(1 != 2)
+console.log('1' != 1)
+console.log('1' != 2)
