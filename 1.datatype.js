f6fb334385c3c1d5891603dd847196ef39740f30
Je Yun <alwaysblue21@gmail.com>
2023-12-07 06:39:16 -0500

    primes

---
+++ 1.datatype.js
@@ -0,0 +1,30 @@
+// primes //
+
+// string
+// anything inside "", '', or ``
+
+// number
+// examples: 1, 1.1, -1, -1.1, infinity, -infinity or NaN
+// (lets keep things simple and just use the regulars numbers for now)
+
+// boolean
+// true or false
+
+// null
+let a = null
+console.log(a)
+
+// undefined
+let b;
+console.log(b)
+
+/////////////////////////////////////////
+//console log a string, number, boolean, null and undefined
+console.log('hello world')
+console.log(123)
+console.log(false)
+console.log(null)
+console.log(undefined)
+
+
+
