525f3622883581385fa0c58c7d0e0df59c909284
Je Yun <alwaysblue21@gmail.com>
2023-12-07 07:43:06 -0500

    operators

---
+++ 2.mathmethods.js
@@ -0,0 +1,43 @@
+// operators and methods
+
+// + addition
+console.log(1 + 1);
+
+// - subtraction
+console.log(1 - 1);
+
+// multiplication x
+console.log(2 * 2);
+
+// look up PEMDAS or order of operations
+
+// Exponentiation **
+console.log(2 ** 3)
+
+// Division /
+console.log(6 / 4)
+
+// Modulus %
+// Division remainder
+console.log(5 % 3)
+
+// Increment ++
+let x = 101;
+x++
+console.log(x)
+
+// Decrement --
+let y = 101;
+y--
+console.log(y)
+
+
+// Increment by value
+let z = 101;
+z+=10
+console.log(z)
+
+// Decrement by value -=
+let aa = 101;
+aa-=10
+console.log(aa)
